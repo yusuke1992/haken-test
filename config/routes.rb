0a089d3d85adda12c1e232c6281ab9aa38ff8715
原田悠佑 <adsoofmelk1327@gmail.com>
2018-04-23 15:02:30 +0900

    resourcesにdestroyを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'jobads#index'
 
-  resources :jobads, only: [ :index, :new, :create, :show]
+  resources :jobads, only: [ :index, :new, :create, :show, :destroy]
 end
